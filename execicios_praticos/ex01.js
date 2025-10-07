bb0faf86f57e0763820af4e90b93aac0e30913ad
gwilhermealm <guilhermealmeidze@gmail.com>
2025-10-06 23:38:23 -0300

    hjj

---
+++ execicios_praticos/ex01.js
@@ -0,0 +1,18 @@
+//fundamentos (variaveis,tipos e operadores )
+let nome = "João"
+let idade = 18
+estaAtivo = false
+//1. Variáveis e Saída: 
+console.log(`ola ${nome} voce tem ${idade} anos e nao esta ativo ${estaAtivo}`)
+console.log(typeof nome, typeof idade, typeof estaAtivo)
+//2. Cálculo Aritmético Simples: 
+let valor1 = 45
+let valor2 = 25
+let soma = valor1 + valor2
+let multiplicacao=valor1*valor2
+let divisao= valor1/valor2
+let resto=valor1%valor2
+let subtracao=valor1-valor2
+
+console.log(`a soma: ${soma}\n multiplicaçao: ${multiplicacao}\n divisao: ${divisao}\n resto: ${resto}\n subtraçao: ${subtracao} `)
+
